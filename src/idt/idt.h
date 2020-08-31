57a22c5019d1961391e6979af56dad36adb3cc14
Chrapati <des.abeilles.et.du.miel.24@gmail.com>
2020-08-31 11:12:16 +0200

    Problem with idt.c

--- src/idt/idt.h
+++ src/idt/idt.h
@@ -15,4 +15,16 @@ struct IDTDescription
     uint32_t zero;
 };
 
+struct IDT_entry
+{
+    unsigned short int offset_lowerbits;
+    unsigned short int selector;
+    unsigned char zero;
+    unsigned char type_attr;
+    unsigned short int offset_higherbits;
+};
+
+struct IDT_entry IDT[256];
+
+
 
